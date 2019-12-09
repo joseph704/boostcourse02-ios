dde4c6c1775b5d523b859b026238eaa7cea4a913
joseph704 <freelove54@naver.com>
2019-12-09 16:03:37 +0900

    Initial Commit

---
+++ boostcouse02-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  boostcouse02-iOS
+//
+//  Created by 차요셉 on 09/12/2019.
+//  Copyright © 2019 차요셉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
