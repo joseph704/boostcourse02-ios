861455fab294c02fd6790d3db1177dd328de6610
joseph704 <freelove54@naver.com>
2020-01-09 21:03:04 +0900

    Add function of Buttons and PhonenumberTextField in ThirdViewController.swift

--- boostcouse02-iOS/ThirdViewController.swift
+++ boostcouse02-iOS/ThirdViewController.swift
@@ -12,6 +12,8 @@ class ThirdViewController: UIViewController {
     @IBOutlet weak var pickerView: UIDatePicker!
     @IBOutlet weak var myView: UILabel!
     @IBOutlet weak var textView: UITextField!
+    @IBOutlet weak var phoneNumber: UITextField!
+    @IBOutlet weak var signupButton: UIButton!
     
     
     
@@ -25,11 +27,35 @@ class ThirdViewController: UIViewController {
         myView.text = dateformatter.string(from: pickerView.date)
         
     }
+    // 취소 버튼 클릭시 action method
+    @IBAction func cancelButtonClicked(_ sender: Any) {
+        // 첫 화면으로 이동
+        self.view.window?.rootViewController?.dismiss(animated: true, completion: nil)
+    }
+    
+    // 이전 버튼 클릭시 action method
+    @IBAction func beforeButtonClicked(_ sender: Any) {
+        // 이전 화면으로 이동
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    // 가입 버튼 클릭시 action method
+    @IBAction func signupButtonClicked(_ sender: Any) {
+        // 싱클턴 클래스 메소드에 삽입
+        UserInformation.shared.birth=myView.text
+        UserInformation.shared.phonenumber=phoneNumber.text
+        // 첫화면으로 이동
+        self.view.window?.rootViewController?.dismiss(animated: true, completion: nil)
+    }
     
     // DatePicker 액션 메소드
     @IBAction func changeDatePikcer(_ sender: Any) {
         pickerView.addTarget(self, action: #selector(changed), for: .valueChanged)
 
+    }
+    @IBAction func tapView(_ sender: Any) {
+        self.view.endEditing(true)
+        
     }
     
     // pickerView action 메소드
@@ -42,6 +68,11 @@ class ThirdViewController: UIViewController {
         let date = dateformatter.string(from: pickerView.date)
         // 포매팅한 값을 myView 라벨에 대입
         myView.text = date
+        
+        // 전화번호 textView가 채워졌을 시
+        if textView.text != "" {
+            signupButton.isEnabled = true
+        }
     }
     
     /*
